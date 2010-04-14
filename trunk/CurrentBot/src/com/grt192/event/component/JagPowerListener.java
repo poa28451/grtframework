312aabee39120f15186c474dd0b670f1928b0d30
spartango <spartango@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2010-04-14 08:51:32 +0000

    clean for sync :

--- trunk/CurrentBot/src/com/grt192/event/component/JagPowerListener.java
+++
@@ -1,7 +0,0 @@
-package com.grt192.event.component;
-
-public interface JagPowerListener {
-	public void currentChanged(JagPowerEvent e);
-	public void voltageChanged(JagPowerEvent e);
-	public void temperatureChanged(JagPowerEvent e);
-}
--- trunk/CurrentBot/src/com/grt192/event/component/JagSwitchListener.java
+++
@@ -1,8 +0,0 @@
-package com.grt192.event.component;
-
-public interface JagSwitchListener {
-	public void leftSwitchPressed(JagSwitchEvent e);
-	public void leftSwitchReleased(JagSwitchEvent e);
-	public void rightSwitchPressed(JagSwitchEvent e);
-	public void rightSwitchReleased(JagSwitchEvent e);
-}
