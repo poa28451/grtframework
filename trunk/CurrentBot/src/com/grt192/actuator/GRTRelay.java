62e6c539b5084ee810923f55e5e780c0f6264686
Vikramsunder25 <Vikramsunder25@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2010-04-25 06:15:18 +0000

    Added Javadoc stuff to all actuator classes except for GRTCANJaguar and GRTDriveTrain

--- trunk/CurrentBot/src/com/grt192/actuator/GRTRelay.java
+++ trunk/CurrentBot/src/com/grt192/actuator/GRTRelay.java
@@ -15,11 +15,18 @@ public class GRTRelay extends Actuator {
     public static final double RELAY_OFF = 0.0;
     private Relay relay;
     
-
+    /**
+     * Creates relay and sets it to a port number on the digital side car as specified
+     * @param channel
+     */
     public GRTRelay(int channel) {
         relay = new Relay(channel);
     }
-
+/**
+ * Sends command to relay.  If the value of the command is -1.0 the relay is in reverse,
+ * 1.0 is forward, and 0.0 is off.
+ * @param c
+ */
     public void executeCommand(Command c) {
         if (c.getValue() == RELAY_OFF) {
             relay.set(Relay.Value.kOff);
@@ -29,11 +36,16 @@ public class GRTRelay extends Actuator {
             relay.set(Relay.Value.kReverse);
         }
     }
-
+/**
+ * SEts the state of the relay to off
+ */
     public void halt() {
         relay.set(Relay.Value.kOff);
     }
-
+/**
+ * Returns String
+ * @return
+ */
     public String toString() {
         return "Relay";
     }
--- trunk/CurrentBot/src/com/grt192/actuator/GRTServo.java
+++ trunk/CurrentBot/src/com/grt192/actuator/GRTServo.java
@@ -16,10 +16,21 @@ public class GRTServo extends Actuator {
 
     private Servo servo;
 
+    /**
+     * Creates servo and sets it to a port number on the digital side car as specified
+     * @param channel
+     */
     public GRTServo(int channel) {
         servo = new Servo(channel);
     }
 
+    /**
+     * Executes a command. The command should have a value betweene 0 and 360.0 to
+     * set the angle of the servo.  Note that you can send values to your servo that
+     * it cannot get to.
+     *
+     * @param c
+     */
     public void executeCommand(Command c) {
         double value = c.getValue();
         if (value > 0.0 && value < 360.0) {
@@ -27,10 +38,17 @@ public class GRTServo extends Actuator {
         }
     }
 
+    /**
+     * The servo returns to angle 90 degrees
+     */
     public void halt() {
         servo.setAngle(90);
     }
 
+    /**
+     * 
+     * @return
+     */
     public String toString() {
         return "Servo";
     }
