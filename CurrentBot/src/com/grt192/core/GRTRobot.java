0d3dd3847b4b5e1d5f5f2616025587975e64e2f6
andrewtheannihilator <andrewtheannihilator@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2010-11-04 00:16:58 +0000

    added GRTRobot GRTLogger accessor

--- CurrentBot/src/com/grt192/core/GRTRobot.java
+++ CurrentBot/src/com/grt192/core/GRTRobot.java
@@ -14,120 +14,123 @@ import edu.wpi.first.wpilibj.SimpleRobot;
  * @author anand, ajc
  */
 public abstract class GRTRobot extends SimpleRobot {
-	// Shared objects
+    // Shared objects
 
-	protected Hashtable globals;
-	protected Vector autonomousControllers;
-	protected Vector teleopControllers;
-	protected Vector globalListeners;
-	protected static GRTRobot instance;
-	private WatchDogController watchDogCtl;
-        private GRTLogger logger;
+    protected Hashtable globals;
+    protected Vector autonomousControllers;
+    protected Vector teleopControllers;
+    protected Vector globalListeners;
+    protected static GRTRobot instance;
+    private WatchDogController watchDogCtl;
+    private GRTLogger logger;
 
-	public GRTRobot() {
-		globals = new Hashtable();
-		autonomousControllers = new Vector();
-		teleopControllers = new Vector();
+    public GRTRobot() {
+        globals = new Hashtable();
+        autonomousControllers = new Vector();
+        teleopControllers = new Vector();
 
-		globalListeners = new Vector();
-		watchDogCtl = new WatchDogController(getWatchdog());
-		watchDogCtl.start();
-                logger = new GRTLogger();
-		System.out.println("Started GRT Framework");
-		instance = this;
-	}
+        globalListeners = new Vector();
+        watchDogCtl = new WatchDogController(getWatchdog());
+        watchDogCtl.start();
+        logger = new GRTLogger();
+        System.out.println("Started GRT Framework");
+        instance = this;
+    }
 
-	public synchronized Hashtable getGlobals() {
-		return globals;
-	}
+    public synchronized Hashtable getGlobals() {
+        return globals;
+    }
 
-	/**
-	 * Get an object shared between controllers
-	 * 
-	 * @param key
-	 * @return value
-	 */
-	public synchronized Object getGlobal(String key) {
-		return globals.get(key);
-	}
+    /**
+     * Get an object shared between controllers
+     *
+     * @param key
+     * @return value
+     */
+    public synchronized Object getGlobal(String key) {
+        return globals.get(key);
+    }
 
-	/**
-	 * Add an object to be shared between controllers
-	 * 
-	 * @param key
-	 * @param value
-	 */
-	public synchronized void putGlobal(String key, Object value) {
-		globals.put(key, value);
-		notifyGlobalListeners(key);
-	}
+    /**
+     * Add an object to be shared between controllers
+     *
+     * @param key
+     * @param value
+     */
+    public synchronized void putGlobal(String key, Object value) {
+        globals.put(key, value);
+        notifyGlobalListeners(key);
+    }
 
-	/**
-	 * This method is called when the robot enters Autonomous mode if any Teleop
-	 * controllers are started, they should be stopped then, all autonomous
-	 * controllers are started
-	 */
-	public void autonomous() {
-		System.out.println("AUTO");
-		// Stop teleopcontrollers if started
-		for (int i = 0; i < teleopControllers.size(); i++) {
-			Controller c = (Controller) teleopControllers.elementAt(i);
-			if (c.isRunning()) {
-				c.stopControl();
-			}
-		}
+    /**
+     * This method is called when the robot enters Autonomous mode if any Teleop
+     * controllers are started, they should be stopped then, all autonomous
+     * controllers are started
+     */
+    public void autonomous() {
+        System.out.println("AUTO");
+        // Stop teleopcontrollers if started
+        for (int i = 0; i < teleopControllers.size(); i++) {
+            Controller c = (Controller) teleopControllers.elementAt(i);
+            if (c.isRunning()) {
+                c.stopControl();
+            }
+        }
 
-		// Start
-		for (int i = 0; i < autonomousControllers.size(); i++) {
-			Controller c = (Controller) autonomousControllers.elementAt(i);
-			if (!c.isRunning()) {
-				c.start();
-			}
-		}
+        // Start
+        for (int i = 0; i < autonomousControllers.size(); i++) {
+            Controller c = (Controller) autonomousControllers.elementAt(i);
+            if (!c.isRunning()) {
+                c.start();
+            }
+        }
 
-	}
+    }
 
-	/**
-	 * This method is called when the robot enters Operator Control mode if any
-	 * Autonomous controllers are started, they should be stopped then, all
-	 * teleop controllers are started
-	 */
-	public void operatorControl() {
-		// Stop AutonomousControllers if started
-		System.out.println("OP");
-		for (int i = 0; i < autonomousControllers.size(); i++) {
-			Controller c = (Controller) autonomousControllers.elementAt(i);
-			if (c.isRunning()) {
-				c.stopControl();
-			}
-		}
+    /**
+     * This method is called when the robot enters Operator Control mode if any
+     * Autonomous controllers are started, they should be stopped then, all
+     * teleop controllers are started
+     */
+    public void operatorControl() {
+        // Stop AutonomousControllers if started
+        System.out.println("OP");
+        for (int i = 0; i < autonomousControllers.size(); i++) {
+            Controller c = (Controller) autonomousControllers.elementAt(i);
+            if (c.isRunning()) {
+                c.stopControl();
+            }
+        }
 
-		// Start
-		for (int i = 0; i < teleopControllers.size(); i++) {
-			Controller c = (Controller) teleopControllers.elementAt(i);
-			if (!c.isRunning()) {
-				c.start();
-			}
-		}
-	}
+        // Start
+        for (int i = 0; i < teleopControllers.size(); i++) {
+            Controller c = (Controller) teleopControllers.elementAt(i);
+            if (!c.isRunning()) {
+                c.start();
+            }
+        }
+    }
 
-	protected void notifyGlobalListeners(String key) {
-		for (int i = 0; i < globalListeners.size(); i++) {
-			((GlobalListener) globalListeners.elementAt(i))
-					.globalChanged(new GlobalEvent(GlobalEvent.DEFAULT, key,
-							globals));
-		}
-	}
+    protected void notifyGlobalListeners(String key) {
+        for (int i = 0; i < globalListeners.size(); i++) {
+            ((GlobalListener) globalListeners.elementAt(i)).globalChanged(new GlobalEvent(GlobalEvent.DEFAULT, key,
+                    globals));
+        }
+    }
 
-	public void addGlobalListener(GlobalListener gl) {
-		this.globalListeners.addElement(gl);
-	}
+    public void addGlobalListener(GlobalListener gl) {
+        this.globalListeners.addElement(gl);
+    }
 
-	public void removeGlobalListener(GlobalListener gl) {
-		this.globalListeners.removeElement(gl);
-	}
+    public void removeGlobalListener(GlobalListener gl) {
+        this.globalListeners.removeElement(gl);
+    }
 
-	public static GRTRobot getInstance() {
-		return instance;
-	}
+    public static GRTRobot getInstance() {
+        return instance;
+    }
+
+    public GRTLogger getLogger() {
+        return logger;
+    }
 }
