3951bd7875d782076afc682f2b416c7ef535ba3b
andrewtheannihilator <andrewtheannihilator@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2011-01-24 06:36:44 +0000

    Removed CommandArray class to remove confusion-- All Commands can now hold more than 1 datum

--- trunk/CurrentBot/src/com/grt192/core/Command.java
+++ trunk/CurrentBot/src/com/grt192/core/Command.java
@@ -4,24 +4,44 @@ package com.grt192.core;
  * Message carrying object that represents an order for a specific actuator
  * @author Ryo
  */
-public class Command {
+public final class Command {
 
-    private double value;
+    private double[] values;
     private int sleepTime;
     private boolean atomic;
 
-    public Command(double value){
-        this(value, 0);
+    public Command(double value) {
+        this(new double[]{value});
+    }
+
+    public Command(double value1, double value2) {
+        this(new double[]{value1, value2});
+    }
+
+    public Command(double value1, double value2, double value3) {
+        this(new double[]{value1, value2, value3});
     }
 
     public Command(double value, int sleepTime){
-        this(value, sleepTime, false);
+        this(new double[]{value}, sleepTime, false);
     }
 
-    public Command(double value, int sleepTime, boolean reset) {
-        this.value = value;
+    public Command(double value, int sleepTime, boolean atomic) {
+        this(new double[]{value}, sleepTime, atomic);
+    }
+
+    public Command(double[] values) {
+        this(values, 0);
+    }
+
+    public Command(double[] values, int sleepTime) {
+        this(values, sleepTime, false);
+    }
+
+    public Command(double[] values, int sleepTime, boolean atomic) {
+        this.values = values;
         this.sleepTime = sleepTime;
-        this.atomic = reset;
+        this.atomic = atomic;
     }
 
     public int getSleepTime() {
@@ -29,12 +49,18 @@ public class Command {
     }
 
     public double getValue() {
-        return value;
+        return getValue(0);
+    }
+
+    public double getValue(int index) {
+        return values[index];
+    }
+
+    public int size() {
+        return values.length;
     }
 
     public boolean isAtomic() {
         return atomic;
     }
-
-    
 }
