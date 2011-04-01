c3e2855425c4a918197584720096276585f0d927
andrewtheannihilator <andrewtheannihilator@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2011-04-01 12:52:39 +0000

    Command documented, reverted to single datum

--- trunk/CurrentBot/src/com/grt192/core/Command.java
+++ trunk/CurrentBot/src/com/grt192/core/Command.java
@@ -2,64 +2,66 @@ package com.grt192.core;
 
 /**
  * Message carrying object that represents an order for a specific actuator
+ * @see Actuator
  * @author Ryo
  */
-public final class Command {
+public class Command {
 
-    private double[] values;
+    private double value;
     private int sleepTime;
     private boolean atomic;
 
+    /**
+     * Constructs a new <code>Command</code> with zero sleep time that
+     * doesn't halt the actuator
+     * @param value
+     */
     public Command(double value) {
-        this(new double[]{value});
+        this(value, 0);
     }
 
-    public Command(double value1, double value2) {
-        this(new double[]{value1, value2});
+    /**
+     * Constructs a new <code>Command</code>  that ensures <code>sleepTime</code>
+     * amount of actuator run time that doesn't halt the actuator
+     * @param value
+     * @param sleepTime minimum actuation time
+     */
+    public Command(double value, int sleepTime) {
+        this(value, sleepTime, false);
     }
 
-    public Command(double value1, double value2, double value3) {
-        this(new double[]{value1, value2, value3});
-    }
-
-    public Command(double value, int sleepTime){
-        this(new double[]{value}, sleepTime, false);
-    }
-
-    public Command(double value, int sleepTime, boolean atomic) {
-        this(new double[]{value}, sleepTime, atomic);
-    }
-
-    public Command(double[] values) {
-        this(values, 0);
-    }
-
-    public Command(double[] values, int sleepTime) {
-        this(values, sleepTime, false);
-    }
-
-    public Command(double[] values, int sleepTime, boolean atomic) {
-        this.values = values;
+    /**
+     * Constructs a new <code>Command</code>  that ensures <code>sleepTime</code>
+     * amount of actuator run time that may halt the actuator
+     * @param value
+     * @param sleepTime minimum actuation time
+     * @param reset halt actuator after execution and sleep
+     */
+    public Command(double value, int sleepTime, boolean reset) {
+        this.value = value;
         this.sleepTime = sleepTime;
         this.atomic = atomic;
     }
 
+    /**
+     * Sets the amount of time that the Command will be enabled for
+     * before executing next command in queue
+     */
+    public void setSleepTime(int sleepTime) {
+        this.sleepTime = sleepTime;
+    }
+
+    /** Gets the amount guaranteed execution time **/
     public int getSleepTime() {
         return sleepTime;
     }
 
+    /** Gets the value, which stores command data as read by actuators **/
     public double getValue() {
-        return getValue(0);
+        return value;
     }
 
-    public double getValue(int index) {
-        return values[index];
-    }
-
-    public int size() {
-        return values.length;
-    }
-
+    /** Tests if this command is atomic: executed followed by actuator halt **/
     public boolean isAtomic() {
         return atomic;
     }
