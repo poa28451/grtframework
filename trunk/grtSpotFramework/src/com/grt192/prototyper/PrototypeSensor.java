9f72948e8b5af49df28af942e6f3c0005c934cb8
andrewtheannihilator <andrewtheannihilator@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2011-05-08 19:41:16 +0000

    Added basestation compatibility, documentation for Prototyper package

--- trunk/grtSpotFramework/src/com/grt192/prototyper/PrototypeSensor.java
+++ trunk/grtSpotFramework/src/com/grt192/prototyper/PrototypeSensor.java
@@ -4,47 +4,91 @@ import com.grt192.core.Sensor;
 import java.util.Vector;
 
 /**
- * Sends events on a host going online/offline
+ * Sends events on prototyped hosts going online/offline
+ * Note: this sensor does not make use of Sensor state
  * @author ajc
  */
 public class PrototypeSensor extends Sensor {
 
-    private boolean[] oldState;
+    //maps prototype to prototype.isUp()
+    private boolean[] oldState;     
     private Prototyper p;
+    //PrototypeingListeners to notify on event
     private Vector listeners;
 
-    public PrototypeSensor() {
-        p = Prototyper.getInstance();
+    PrototypeSensor(Prototyper p) {
+        this.p = p;
         oldState = new boolean[p.getHosts().length];
         listeners = new Vector();
     }
 
     public void poll() {
         for (int i = 0; i < p.getHosts().length; i++) {
-            boolean up = p.getHost(i).isUp();
+            PrototypedHost h = p.getHost(i);
+            boolean up = h.isUp();
             if (oldState[i] != up) {
-                notifyListeners(p.getHost(i).getAnAddress(), i, up);
+                notifyListeners(h.getAnAddress(), i, up);
                 oldState[i] = up;
             }
         }
     }
 
+    /** Gets all online prototyped hosts */
+    public PrototypedHost[] getOnlineHosts() {
+        //store online hosts
+        Vector upHosts = new Vector();
+        //loop through all hosts
+        for (int i = 0; i < p.getHosts().length; i++) {
+            PrototypedHost host = p.getHost(i);
+            if (host.isUp()) {
+                upHosts.addElement(host);
+            }
+        }
+
+        PrototypedHost[] retu = new PrototypedHost[upHosts.size()];
+        upHosts.copyInto(retu);
+        return retu;
+    }
+
+    /** Prints data of all  online prototyped hosts */
+    public void printStatus() {
+        System.out.println("HOSTS:");
+        PrototypedHost[] onlineHosts = getOnlineHosts();
+        for (int i = 0; i < onlineHosts.length; i++) {
+            System.out.println(
+                    "\t["
+                    + onlineHosts[i].getPrototype()
+                    + ":"
+                    + onlineHosts[i].getAddress()
+                    + "]");
+
+        }
+    }
+
+    /** 
+     * Notifies <code>PrototypeingListener</code>s on prototyping event
+     * @param host host affected
+     * @param prototype prototype affected
+     * @param up new state
+     */
     public void notifyListeners(String host, int prototype, boolean up) {
         if (up) {
             for (int i = 0; i < listeners.size(); i++) {
-                ((PrototypeingListener) listeners.elementAt(i)).hostUp(i, host);
+                ((PrototypeingListener) listeners.elementAt(i)).hostUp(prototype, host);
             }
         } else {
             for (int i = 0; i < listeners.size(); i++) {
-                ((PrototypeingListener) listeners.elementAt(i)).hostDown(i, host);
+                ((PrototypeingListener) listeners.elementAt(i)).hostDown(prototype, host);
             }
         }
     }
 
+    /** Adds provided <code>PrototypeingListener</code> to be called on prototypeing events */
     public void addPrototypeListener(PrototypeingListener l) {
         listeners.addElement(l);
     }
 
+    /** Removes provided <code>PrototypeingListener</code> from being called on prototypeing events */
     public void removePrototypeListener(PrototypeingListener l) {
         listeners.removeElement(l);
     }
