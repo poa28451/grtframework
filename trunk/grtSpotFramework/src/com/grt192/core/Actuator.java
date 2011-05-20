ee69ec7556ae5e6b43b6649acbff78a1212d5532
andrewtheannihilator <andrewtheannihilator@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2011-05-20 00:40:35 +0000

    Added enqueueFreeCommand(), which only enters command to queue if there is room for the command (if the queue is nearly empty)

--- trunk/grtSpotFramework/src/com/grt192/core/Actuator.java
+++ trunk/grtSpotFramework/src/com/grt192/core/Actuator.java
@@ -4,237 +4,295 @@
  */
 package com.grt192.core;
 
+import java.util.Vector;
+
 import com.grt192.actuator.exception.ActuatorException;
 import com.grt192.event.ActuatorCommandListener;
 import com.grt192.event.ActuatorEvent;
-import java.util.Vector;
 
 /**
- * Superclass for all specific actuators on a robot.
- * Runs continously, waiting for commands. A driver of sorts
+ * Superclass for all specific actuators on a robot. Runs jkj2continously,
+ * waiting for commands. A driver of sorts
+ * 
  * @author anand
  */
 public abstract class Actuator extends GRTObject {
 
-    protected static final int MAGIC = 999; //protects actuators from overriding
-    //this must be passed to clear a command queue
-    public static final int SLEEP_INTERVAL = 5; //ms
-    private boolean running;
-    private boolean suspended;
-    private Vector commands;  //Queue of commands to be executed
-    private Command current;  //Current command being executed
-    private Vector actuatorCommandListeners;
+	protected static final int MAGIC = 999; // protects actuators from
+											// overriding
+	// this must be passed to clear a command queue
+	public static final int SLEEP_INTERVAL = 5; // ms
+	private boolean running;
+	private boolean suspended;
+	private Vector commands; // Queue of commands to be executed
+	private Command current; // Current command being executed
+	private Vector actuatorCommandListeners;
 
-    public Actuator() {
-        commands = new Vector();
-        current = null;
-        running = false;
-        suspended = false;
-        actuatorCommandListeners = new Vector();
-    }
+	public Actuator() {
+		commands = new Vector();
+		current = null;
+		running = false;
+		suspended = false;
+		actuatorCommandListeners = new Vector();
+	}
 
-    /**
-     * Start continuous execution of commands from the command queue
-     * Actuator will wait for a command to be present and execute it
-     * if suspended the actuator will continue running but ignore commands
-     * on exception the actuator will halt safely
-     */
-    public void run() {
-        running = true;
-        while (running) {
-            try {
-                if (!suspended) {
-                    current = dequeue();
-                    if (current != null) {
-                        //Issue order to hardware for operation
-                        executeCommand(current);
-                        //Allow for hardware operation delays
-                        if (current.getSleepTime() > 0) {
-                            sleep(current.getSleepTime());
-                            if (current.isAtomic()) {
-                                this.halt();
-                            }
-                        }
-                        for (int i = 0; i < actuatorCommandListeners.size(); i++) {
-                            ((ActuatorCommandListener) actuatorCommandListeners.elementAt(i)).commandDidComplete(
-                                    new ActuatorEvent(this,
-                                    ActuatorEvent.COMMAND_COMPLETE,
-                                    this.current));
-                        }
+	/**
+	 * Start continuous execution of commands from the command queue Actuator
+	 * will wait for a command to be present and execute it if suspended the
+	 * actuator will continue running but ignore commands on exception the
+	 * actuator will halt safely
+	 */
+	public void run() {
+		running = true;
+		while (running) {
+			try {
+				if (!suspended) {
+					current = dequeue();
+					if (current != null) {
+						// Issue order to hardware for operation
+						executeCommand(current);
+						// Allow for hardware operation delays
+						if (current.getSleepTime() > 0) {
+							sleep(current.getSleepTime());
+							if (current.isAtomic()) {
+								this.halt();
+							}
+						}
+						for (int i = 0; i < actuatorCommandListeners.size(); i++) {
+							((ActuatorCommandListener) actuatorCommandListeners
+									.elementAt(i))
+									.commandDidComplete(new ActuatorEvent(this,
+											ActuatorEvent.COMMAND_COMPLETE,
+											this.current));
+						}
 
-                    }
-                }
-                //minimum loop sleep
-                sleep(SLEEP_INTERVAL);
-            } catch (Exception e) {
-                //On exception kill this actuator, as it is
-                //unsafe to continue operation
-                for (int i = 0; i < actuatorCommandListeners.size(); i++) {
-                    ((ActuatorCommandListener) actuatorCommandListeners.elementAt(i)).commandDidComplete(
-                            new ActuatorEvent(this,
-                            ActuatorEvent.COMMAND_FAILED,
-                            this.current));
-                }
-                e.printStackTrace();
-                stopActuator();
-            }
-        }
-    }
+					}
+				}
+				// minimum loop sleep
+				sleep(SLEEP_INTERVAL);
+			} catch (Exception e) {
+				// On exception kill this actuator, as it is
+				// unsafe to continue operation
+				for (int i = 0; i < actuatorCommandListeners.size(); i++) {
+					((ActuatorCommandListener) actuatorCommandListeners
+							.elementAt(i))
+							.commandDidComplete(new ActuatorEvent(this,
+									ActuatorEvent.COMMAND_FAILED, this.current));
+				}
+				e.printStackTrace();
+				stopActuator();
+			}
+		}
+	}
 
-    /**
-     * Executes the command next command. Only called from the Run method.
-     * Each actuator must have this method.
-     * @param c
-     */
-    protected abstract void executeCommand(Command c) throws ActuatorException;
+	/**
+	 * Executes the command next command. Only called from the Run method. Each
+	 * actuator must have this method.
+	 * 
+	 * @param c
+	 */
+	protected abstract void executeCommand(Command c) throws ActuatorException;
 
-    public Vector getCommands() {
-        return commands;
-    }
+	public Vector getCommands() {
+		return commands;
+	}
 
-    /**
-     * Add a command to be executed by this actuator
-     * @param c
-     */
-    public synchronized void enqueueCommand(double c) {
-        commands.addElement(new Command(c));
-    }
+	/**
+	 * Adds a command to be executed by this actuator
+	 * 
+	 * @param c
+	 */
+	public synchronized void enqueueCommand(double c) {
+		commands.addElement(new Command(c));
+	}
 
-    public synchronized void enqueueCommand(Command c) {
-        commands.addElement(c);
-    }
+	public synchronized void enqueueCommand(Command c) {
+		commands.addElement(c);
+	}
 
-    /**
-     * Add a series of command to be executed in order by this Actuator
-     * @param commandList
-     */
-    public synchronized void enqueueCommands(Vector commandList) {
-        for (int i = 0; i < commandList.size(); i++) {
-            enqueueCommand((Command) commandList.elementAt(i));
-        }
-    }
+	/**
+	 * Adds a command to be executed by this actuator only if there is at most 1
+	 * command in queue.
+	 * 
+	 * @param c
+	 *            command to be excecuted
+	 */
+	public synchronized void enqueueFreeCommand(Command c) {
+		if (isFree(1)) {
+			commands.addElement(c);
+		}
+	}
 
-    public synchronized void enqueueCommands(Command[] commandList) {
-        for (int i = 0; i < commandList.length; i++) {
-            enqueueCommand(commandList[i]);
-        }
-    }
+	/**
+	 * Add a series of command to be executed in order by this Actuator
+	 * 
+	 * @param commandList
+	 */
+	public synchronized void enqueueCommands(Vector commandList) {
+		for (int i = 0; i < commandList.size(); i++) {
+			enqueueCommand((Command) commandList.elementAt(i));
+		}
+	}
 
-    public synchronized void enqueueCommands(double[] commandList) {
-        for (int i = 0; i < commandList.length; i++) {
-            enqueueCommand(commandList[i]);
-        }
-    }
+	public synchronized void enqueueCommands(Command[] commandList) {
+		for (int i = 0; i < commandList.length; i++) {
+			enqueueCommand(commandList[i]);
+		}
+	}
 
-    /**
-     * remove a specific command from the actuator's queue without executing
-     * @param c
-     */
-    public void removeCommand(Command c) {
-        commands.removeElement(c);
-    }
+	public synchronized void enqueueCommands(double[] commandList) {
+		for (int i = 0; i < commandList.length; i++) {
+			enqueueCommand(commandList[i]);
+		}
+	}
 
-    /**
-     * Remove the first item from the command queue
-     * @return command
-     */
-    protected Command dequeue() {
-        if (commands.isEmpty()) {
-            return null;
-        }
-        Command removed = (Command) (commands.elementAt(0));
-        commands.removeElementAt(0);
-        return removed;
-    }
+	/**
+	 * remove a specific command from the actuator's queue without executing
+	 * 
+	 * @param c
+	 */
+	public void removeCommand(Command c) {
+		commands.removeElement(c);
+	}
 
-    /**
-     * Supercedes commands in the queue and does the command provided
-     * @param c
-     * @throws ActuatorException 
-     */
-    public void doCommand(Command c) throws ActuatorException {
-        this.pause();
-        this.executeCommand(c);
-        this.unpause();
-    }
+	/**
+	 * Remove the first item from the command queue
+	 * 
+	 * @return command
+	 */
+	protected Command dequeue() {
+		if (commands.isEmpty()) {
+			return null;
+		}
+		Command removed = (Command) (commands.elementAt(0));
+		commands.removeElementAt(0);
+		return removed;
+	}
 
-    public void doCommand(double c) throws ActuatorException {
-        doCommand(new Command(c));
-    }
+	/**
+	 * Supercedes commands in the queue and does the command provided
+	 * 
+	 * @param c
+	 * @throws ActuatorException
+	 */
+	public void doCommand(Command c) throws ActuatorException {
+		this.pause();
+		this.executeCommand(c);
+		this.unpause();
+	}
 
-    /**
-     * Clear All commands from the queue. 
-     * Use carefully, a magic number is required to force clear the queue.
-     * @param magicNumber
-     */
-    public synchronized void clearQueue(int magicNumber) {
-        if (magicNumber == MAGIC) {
-            commands.removeAllElements();
-        }
-    }
+	public void doCommand(double c) throws ActuatorException {
+		doCommand(new Command(c));
+	}
 
-    /**
-     * get the current command being executed
-     * @return current
-     */
-    public Command getCurrent() {
-        return current;
-    }
+	/**
+	 * Checks if the actuator is free, or has no commands in queue to execute
+	 * 
+	 * @return
+	 */
+	public boolean isFree() {
+		return commands.isEmpty();
+	}
 
-    public boolean isRunning() {
-        return running;
-    }
+	/**
+	 * Checks if the actuator is free to a provided number of commands in queue
+	 * to actuate.
+	 * 
+	 * @param tolerance
+	 *            the maximum number of commands to be in queue
+	 * @return
+	 */
+	public boolean isFree(int tolerance) {
+		return commands.size() <= tolerance;
+	}
 
-    //Safely bring the actuator to a disabled state.
-    protected abstract void halt();
+	/**
+	 * Returns the number of commands in queue for actuation
+	 * 
+	 * @return
+	 */
+	public int getNumCommands() {
+		return commands.size();
+	}
 
-    /**
-     * stopActuator restores hardware to "safe" state. Then stops actuator Thread.
-     */
-    public void stopActuator() {
-        halt();
-        running = false;
-    }
+	/**
+	 * Clear All commands from the queue. Use carefully, a magic number is
+	 * required to force clear the queue.
+	 * 
+	 * @param magicNumber
+	 */
+	public synchronized void clearQueue(int magicNumber) {
+		if (magicNumber == MAGIC) {
+			commands.removeAllElements();
+		}
+	}
 
-    public boolean isSuspended() {
-        return suspended;
-    }
+	/**
+	 * get the current command being executed
+	 * 
+	 * @return current
+	 */
+	public Command getCurrent() {
+		return current;
+	}
 
-    /**
-     * Pause execution of commands in the queue and stop the action of this actuator
-     */
-    public void hold() {
-        halt();
-        this.suspended = true;
-    }
+	public boolean isRunning() {
+		return running;
+	}
 
-    /**
-     * Pause execution of new commands in the queue
-     */
-    public void pause() {
-        this.suspended = true;
-    }
+	// Safely bring the actuator to a disabled state.
+	protected abstract void halt();
 
-    /**
-     * Resume execution of commands in the queue from where Actutator left off
-     */
-    public void unpause() {
-        suspended = false;
-    }
+	/**
+	 * stopActuator restores hardware to "safe" state. Then stops actuator
+	 * Thread.
+	 */
+	public void stopActuator() {
+		halt();
+		running = false;
+	}
 
-    public Vector getActuatorCommandListeners() {
-        return actuatorCommandListeners;
-    }
+	public boolean isSuspended() {
+		return suspended;
+	}
 
-    /**
-     * add a listener to be notify when commands are executed
-     * @param a
-     */
-    public void addActuatorCommandListener(ActuatorCommandListener a) {
-        actuatorCommandListeners.addElement(a);
-    }
+	/**
+	 * Pause execution of commands in the queue and stop the action of this
+	 * actuator
+	 */
+	public void hold() {
+		halt();
+		this.suspended = true;
+	}
 
-    public void removeActuatorCommandListener(ActuatorCommandListener a) {
-        actuatorCommandListeners.removeElement(a);
-    }
+	/**
+	 * Pause execution of new commands in the queue
+	 */
+	public void pause() {
+		this.suspended = true;
+	}
+
+	/**
+	 * Resume execution of commands in the queue from where Actutator left off
+	 */
+	public void unpause() {
+		suspended = false;
+	}
+
+	public Vector getActuatorCommandListeners() {
+		return actuatorCommandListeners;
+	}
+
+	/**
+	 * add a listener to be notify when commands are executed
+	 * 
+	 * @param a
+	 */
+	public void addActuatorCommandListener(ActuatorCommandListener a) {
+		actuatorCommandListeners.addElement(a);
+	}
+
+	public void removeActuatorCommandListener(ActuatorCommandListener a) {
+		actuatorCommandListeners.removeElement(a);
+	}
 }
