d012f6c1c42bb9cd0a31b0df6287f42c532bed6f
andrewtheannihilator <andrewtheannihilator@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2011-09-15 22:47:09 +0000

    Added documentation to the RPCConnection Interface

--- Split-control/src/com/googlecode/grtframework/rpc/RPCConnection.java
+++ Split-control/src/com/googlecode/grtframework/rpc/RPCConnection.java
@@ -2,6 +2,13 @@ package com.googlecode.grtframework.rpc;
 
 import com.googlecode.grtframework.core.EventPublisher;
 
+/**
+ * An RPCConnection is the transport mechanism for sending data between the
+ * computer and the microcontroller
+ * 
+ * @author Andrew Chen <andrewtheannihilator@gmail.com>
+ * 
+ */
 public interface RPCConnection extends EventPublisher {
 
 	public void send(RPCMessage message);
