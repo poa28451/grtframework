78dbe95062debc7d72090abce90941f70257e9b2
andrewtheannihilator <andrewtheannihilator@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2011-10-19 18:35:22 +0000

    Added state definitions to ISwitch

--- trunk/Split-control/src/com/googlecode/grtframework/sensor/ISwitch.java
+++ trunk/Split-control/src/com/googlecode/grtframework/sensor/ISwitch.java
@@ -3,8 +3,10 @@ package com.googlecode.grtframework.sensor;
 import com.googlecode.grtframework.core.Sensor;
 import com.googlecode.grtframework.event.SwitchListener;
 
-
 public interface ISwitch extends Sensor {
+
+	public static final boolean STATE_PRESSED = true;
+	public static final boolean STATE_RELEASED = false;
 
 	public void addSwitchListener(SwitchListener l);
 
