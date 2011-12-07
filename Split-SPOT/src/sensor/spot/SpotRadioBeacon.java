7f7c4063bf1b3aba881f40654b336186ad1f0fa0
andrewtheannihilator <andrewtheannihilator@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2011-12-07 23:33:33 +0000

    added rpc accelerometer class

---
+++ Split-SPOT/src/sensor/spot/SpotRadioBeacon.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sensor.spot;
+
+/**
+ *
+ * @author ajc
+ */
+public class SpotRadioBeacon {
+
+}
