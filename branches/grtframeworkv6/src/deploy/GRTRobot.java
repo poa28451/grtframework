a22d983e3ccb50b5aaa8121c82058b023c5cfff9
andrewtheannihilator <andrewtheannihilator@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2011-12-23 03:10:10 +0000

    controllers completely implemented

--- branches/grtframeworkv6/src/deploy/GRTRobot.java
+++ branches/grtframeworkv6/src/deploy/GRTRobot.java
@@ -4,12 +4,11 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package deploy;
-
 
 import core.EventController;
 import edu.wpi.first.wpilibj.SimpleRobot;
+import java.util.Vector;
 
 /**
  *
@@ -23,16 +22,27 @@ import edu.wpi.first.wpilibj.SimpleRobot;
  */
 public abstract class GRTRobot extends SimpleRobot {
 
-    
-    public GRTRobot(){
-        
+    private Vector autoControllers;
+    private Vector teleopControllers;
+
+    public GRTRobot() {
+        autoControllers = new Vector();
+        teleopControllers = new Vector();
+
     }
+
     /**
      * This function is called once each time the robot enters autonomous mode.
      * All Autonomous controllers are started/resumed, and all teleop controllers are paused.
      */
     public void autonomous() {
-        
+        for (int i = 0; i < teleopControllers.size(); i++) {
+            ((EventController) teleopControllers.elementAt(i)).disable();
+        }
+        for (int i = 0; i < autoControllers.size(); i++) {
+            ((EventController) autoControllers.elementAt(i)).enable();
+        }
+
     }
 
     /**
@@ -40,14 +50,20 @@ public abstract class GRTRobot extends SimpleRobot {
      * All Teleop controllers are started/resumed, and all autonomous controllers are paused.
      */
     public void operatorControl() {
-
-    }
-
-    public void addAutonomousController(EventController c){
-
-    }
-
-    public void addTeleopController(EventController c){
+        for (int i = 0; i < autoControllers.size(); i++) {
+            ((EventController) autoControllers.elementAt(i)).disable();
+        }
+        for (int i = 0; i < teleopControllers.size(); i++) {
+            ((EventController) teleopControllers.elementAt(i)).enable();
+        }
         
+    }
+
+    public void addAutonomousController(EventController c) {
+        autoControllers.addElement(c);
+    }
+
+    public void addTeleopController(EventController c) {
+        teleopControllers.addElement(c);
     }
 }
