9f8a577f1c9173b269db4add84f02bbd0d32b2fd
andrewtheannihilator <andrewtheannihilator@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2011-12-23 07:43:03 +0000

    added and implemented actutor and GRTVictor classes

---
+++ src/actuator/GRTVictor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actuator;
+
+import core.Actuator;
+import edu.wpi.first.wpilibj.Victor;
+
+/**
+ *
+ * @author ajc
+ */
+public class GRTVictor extends Actuator {
+
+    Victor victor;
+
+    public GRTVictor(int id, String name) {
+        super(name);
+        victor = new Victor(id);
+    }
+
+    public void executeCommand(double command) {
+        if (enabled) {
+            victor.set(command);
+        }
+    }
+}
