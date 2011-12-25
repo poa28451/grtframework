5e72102c2611054808074d63f35558e7e19e4c68
andrewtheannihilator <andrewtheannihilator@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2011-12-25 10:22:18 +0000

    added sensor key definitions, implemented generic sensor states on grtdriverstation

--- branches/grtframeworkv6/src/sensor/XBoxJoystick.java
+++ branches/grtframeworkv6/src/sensor/XBoxJoystick.java
@@ -21,28 +21,33 @@ public class XBoxJoystick extends PollingSensor {
     /**
      * Keys of data
      */
-    public static final int BUTTON_0 = 0;
-    public static final int BUTTON_1 = 1;
-    public static final int BUTTON_2 = 2;
-    public static final int BUTTON_3 = 3;
-    public static final int BUTTON_4 = 4;
-    public static final int BUTTON_5 = 5;
-    public static final int BUTTON_6 = 6;
-    public static final int BUTTON_7 = 7;
-    public static final int BUTTON_8 = 8;
-    public static final int BUTTON_9 = 9;
-    public static final int LEFT_X = 10;
-    public static final int LEFT_Y = 11;
-    public static final int RIGHT_X = 12;
-    public static final int RIGHT_Y = 13;
-    public static final int JOYSTICK_ANGLE = 14;
-    public static final int TRIGGER = 15;
-    public static final int PAD = 16;
+    public static final int KEY_BUTTON_0 = 0;
+    public static final int KEY_BUTTON_1 = 1;
+    public static final int KEY_BUTTON_2 = 2;
+    public static final int KEY_BUTTON_3 = 3;
+    public static final int KEY_BUTTON_4 = 4;
+    public static final int KEY_BUTTON_5 = 5;
+    public static final int KEY_BUTTON_6 = 6;
+    public static final int KEY_BUTTON_7 = 7;
+    public static final int KEY_BUTTON_8 = 8;
+    public static final int KEY_BUTTON_9 = 9;
+    public static final int KEY_LEFT_X = 10;
+    public static final int KEY_LEFT_Y = 11;
+    public static final int KEY_RIGHT_X = 12;
+    public static final int KEY_RIGHT_Y = 13;
+    public static final int KEY_JOYSTICK_ANGLE = 14;
+    public static final int KEY_TRIGGER = 15;
+    public static final int KEY_PAD = 16;
     
     private static final int NUM_DATA = 17;
     private static final int NUM_OF_BUTTONS = 10;
+
+    /**
+     * State definitions
+     */
     public static final double PRESSED = TRUE;
     public static final double RELEASED = FALSE;
+
     private final Joystick joystick;
     private final Vector buttonListeners;
     private final Vector joystickListeners;
@@ -60,13 +65,13 @@ public class XBoxJoystick extends PollingSensor {
             //if we measure true, this indicates pressed state
             setState(i, joystick.getRawButton(i) ? PRESSED : RELEASED);
         }
-        setState(LEFT_X, joystick.getX());
-        setState(LEFT_Y, joystick.getY());
-        setState(RIGHT_X, joystick.getRawAxis(4));
-        setState(RIGHT_Y, joystick.getRawAxis(5));
-        setState(JOYSTICK_ANGLE, joystick.getDirectionRadians());
-        setState(TRIGGER, joystick.getZ());
-        setState(PAD, joystick.getRawAxis(6));
+        setState(KEY_LEFT_X, joystick.getX());
+        setState(KEY_LEFT_Y, joystick.getY());
+        setState(KEY_RIGHT_X, joystick.getRawAxis(4));
+        setState(KEY_RIGHT_Y, joystick.getRawAxis(5));
+        setState(KEY_JOYSTICK_ANGLE, joystick.getDirectionRadians());
+        setState(KEY_TRIGGER, joystick.getZ());
+        setState(KEY_PAD, joystick.getRawAxis(6));
     }
 
     protected void notifyListeners(int id, double oldDatum, double newDatum) {
@@ -89,43 +94,43 @@ public class XBoxJoystick extends PollingSensor {
 
             //call various events based on which datum we are
             switch (id) {
-                case LEFT_X: {
+                case KEY_LEFT_X: {
                     for (int i = 0; i < joystickListeners.size(); i++) {
                         ((XboxJoystickListener) joystickListeners.elementAt(i)).leftXAxisMoved(e);
                     }
 
                 }
-                case LEFT_Y: {
+                case KEY_LEFT_Y: {
                     for (int i = 0; i < joystickListeners.size(); i++) {
                         ((XboxJoystickListener) joystickListeners.elementAt(i)).leftYAxisMoved(e);
                     }
                     break;
                 }
-                case RIGHT_X: {
+                case KEY_RIGHT_X: {
                     for (int i = 0; i < joystickListeners.size(); i++) {
                         ((XboxJoystickListener) joystickListeners.elementAt(i)).rightXAxisMoved(e);
                     }
                     break;
                 }
-                case RIGHT_Y: {
+                case KEY_RIGHT_Y: {
                     for (int i = 0; i < joystickListeners.size(); i++) {
                         ((XboxJoystickListener) joystickListeners.elementAt(i)).rightYAxisMoved(e);
                     }
                     break;
                 }
-                case JOYSTICK_ANGLE: {
+                case KEY_JOYSTICK_ANGLE: {
                     for (int i = 0; i < joystickListeners.size(); i++) {
                         ((XboxJoystickListener) joystickListeners.elementAt(i)).leftXAxisMoved(e);
                     }
                     break;
                 }
-                case TRIGGER: {
+                case KEY_TRIGGER: {
                     for (int i = 0; i < joystickListeners.size(); i++) {
                         ((XboxJoystickListener) joystickListeners.elementAt(i)).triggerMoved(e);
                     }
                     break;
                 }
-                case PAD: {
+                case KEY_PAD: {
                     for (int i = 0; i < joystickListeners.size(); i++) {
                         ((XboxJoystickListener) joystickListeners.elementAt(i)).padMoved(e);
                     }
