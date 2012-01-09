1a37298b16eb4bdab74f0826de36d9f64425857f
andrewtheannihilator <andrewtheannihilator@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2012-01-09 06:46:15 +0000

    added robottiltevent placeholders, removed solenoid class bug

--- branches/grtframeworkv6/src/actuator/GRTSolenoid.java
+++ branches/grtframeworkv6/src/actuator/GRTSolenoid.java
@@ -17,33 +17,30 @@ public class GRTSolenoid extends Actuator {
     
     public static final double ON = 1.0;
     public static final double OFF = 0.0;
-    
-    public GRTSolenoid(int slot, int channel, String id){
+
+    public GRTSolenoid(int slot, int channel, String id) {
         super(id);
-        
+
         sol = new Solenoid(slot, channel);
     }
-    
+
     /*
      * Engage or disengage the solenoid
      */
     public void executeCommand(double command) {
-        if (command == ON){
+        if (command == ON) {
             sol.set(true);
-        } else if (command == OFF){
+        } else if (command == OFF) {
             sol.set(false);
         }
     }
-    
-    /*
-     * Implementation of IProcess.halt()
-     * Retracts solenoid and disables Thread
-     */
-    public void halt(){
+
+
+//    @Override
+    public void halt() {
         sol.set(false);
-        
+
         super.halt();
-        
+
     }
-    
 }
---
+++ branches/grtframeworkv6/src/event/RobotTiltEvent.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package event;
+
+import balancer.RobotTiltAccel;
+
+/**
+ *
+ * @author calvin
+ */
+public class RobotTiltEvent {
+
+    public RobotTiltEvent(RobotTiltAccel aThis, int id, double newDatum) {
+    }
+
+}
