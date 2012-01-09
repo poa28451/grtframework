58f4cdb22c9ac6e714f6dccae6aa08f1bf954ff8
andrewtheannihilator <andrewtheannihilator@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2012-01-09 06:46:15 +0000

    added robottiltevent placeholders, removed solenoid class bug

---
+++ src/event/RobotTiltListener.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package event;
+
+/**
+ *
+ * @author calvin
+ */
+public interface RobotTiltListener {
+
+    public void RobotTiltChange(RobotTiltEvent e);
+
+}
