c3e3f3c1efbc34000618880574b1fe45bfd93747
frei.daniel3 <frei.daniel3@6d1077f6-d798-11de-9196-2db6e9c7c7ae>
2012-01-10 23:01:16 +0000

    *UNTESTED* Finish Tilt Sensor.

--- branches/grtframeworkv6/src/balancer/RobotTiltAccel.java
+++ branches/grtframeworkv6/src/balancer/RobotTiltAccel.java
@@ -12,54 +12,64 @@ import sensor.GRTADXL345;
 import event.RobotTiltEvent;
 import event.RobotTiltListener;
 import java.util.Vector;
+
 /**
  *
  * @author calvin
  */
-public class RobotTiltAccel  extends Sensor implements ADXL345Listener {
-    private Vector robotTiltListeners;
+public class RobotTiltAccel extends Sensor implements ADXL345Listener {
 
-    private static final int NUM_DATA = 4;//TODO change
-    private int ANGLE = 1;
+    private Vector robotTiltListeners;
+    private static final int NUM_DATA = 1;//TODO change
+    public static final int KEY_ANGLE = 0;
     private double angle;
     private double xAccel;
     private double yAccel;
     private double zAccel;
+    private final GRTADXL345 accelerometer;
 
-    public RobotTiltAccel(int pollTime, String name){
+    public RobotTiltAccel(GRTADXL345 accelerometer, String name) {
         super(name);
+        this.accelerometer = accelerometer;
         robotTiltListeners = new Vector();
     }
 
-    protected void notifyListeners(int id, double oldDatum, double newDatum) {
-        RobotTiltEvent e = new RobotTiltEvent(this, id, newDatum);
-        for (int i = 0; i < robotTiltListeners.size(); i++) {
-            ((RobotTiltListener) robotTiltListeners.elementAt(i)).RobotTiltChange(e);
-        }
-    }
-    
-    private void updateAngle(){
-        double normalDeviation = Math.sqrt(((xAccel)*(xAccel))+
-            ((yAccel)*(yAccel)));
+//    protected void notifyListeners(int id, double oldDatum, double newDatum) {
+//        RobotTiltEvent e = new RobotTiltEvent(this, id, newDatum);
+//        for (int i = 0; i < robotTiltListeners.size(); i++) {
+//            ((RobotTiltListener) robotTiltListeners.elementAt(i)).RobotTiltChange(e);
+//        }
+//    }
+
+    private void updateAngle() {
+        //magnitude of vector sum of x, y accelerations
+        double normalDeviation = Math.sqrt(((xAccel) * (xAccel))
+                + ((yAccel) * (yAccel)));
+//        double oldAngle = angle;
         angle = MathUtils.atan(normalDeviation / zAccel);
+        //angle is angle between acceleration vector and z axis
+        notifyStateChange(KEY_ANGLE, angle);
     }
-    
-    public double getTilt(){
+
+    public double getTilt() {
         return angle;
     }
-    
-    public void addRobotTiltListeners(RobotTiltListener l){
+
+    public void addRobotTiltListeners(RobotTiltListener l) {
         robotTiltListeners.addElement(l);
     }
-    
-    public void removeRobotTiltListeners(RobotTiltListener l){
+
+    public void removeRobotTiltListeners(RobotTiltListener l) {
         robotTiltListeners.removeElement(l);
     }
 
+    //@Override
     protected void startListening() {
+        accelerometer.addADXL345Listener(this);
     }
 
     protected void stopListening() {
+        accelerometer.removeADXL345Listener(this);
     }
 
     public void XAccelChange(ADXL345Event e) {
